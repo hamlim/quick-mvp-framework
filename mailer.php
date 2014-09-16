83823391f79b1986a4afb7e106aef6d381b2cfbb
hamlim <hamlim@rpi.edu>
2014-09-15 20:20:56 -0400

    final last changes

--- mailer.php
+++ mailer.php
@@ -2,8 +2,9 @@
 	$to = "productmanager@doma.in";
 	$subject = "A new signup for [product]";
 	$message = $_POST["email"] . " is requesting that you add them to be emailed when you launch!";
+	$messagefixed = wordwrap($message, 70);
 	$headers = 'From: productmanager@example.com' . "\r\n" . 'Reply-To: productmanager@example.com' . "\r\n". 'X-Mailer: PHP/' . phpversion();
-	if(mail($to, $subject, $message, $headers)){
+	if(mail($to, $subject, $messagefixed, $headers)){
 		header('Location: index.html');
 	} else {
 		echo "ERROR!";
